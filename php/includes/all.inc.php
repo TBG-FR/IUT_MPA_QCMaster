05bd7c994aecb420df789562aab4bd6399b31808
TBG-FR <tbg-le-nolife@hotmail.fr>
2017-11-10 20:18:54 +0100

    PHP Includes - All & Settings : Creation

---
+++ php/includes/all.inc.php
@@ -0,0 +1,7 @@
+<?php   // 'all.inc.php' ~ Includes everything (Includes & Classes) so that the php pages will have only one file to Include
+
+        require_once('settings.inc.php');
+
+        require_once('../classes/Database.php');
+
+// End of file ~ We don't close the PHP tag here, in order to avoid inserting invisible characters
---
+++ php/includes/settings.inc.php
@@ -0,0 +1,15 @@
+<?php   // 'settings.inc.php' ~ Useful settings for the whole system (classes, functions, etc)
+
+/* ----- Database Configuration ----- */
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "bdd");
+
+/* ----- Another Configuration ----- */
+/*
+    INSERT CONFIG
+    CODE HERE
+*/
+
+// End of file ~ We don't close the PHP tag here, in order to avoid inserting invisible characters
