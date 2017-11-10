ef04573dd655df2cd1a64519609ec8f188621b91
TBG-FR <tbg-le-nolife@hotmail.fr>
2017-11-10 20:19:25 +0100

    PHP Classes - Database : Creation

---
+++ php/classes/Database.php
@@ -0,0 +1,12 @@
+<?php
+
+class Database {
+ 
+private $host = DB_HOST;
+private $user = DB_USER;
+private $pass = DB_PASS;
+private $dbname = DB_NAME;
+    
+}
+
+// End of file ~ We don't close the PHP tag here, in order to avoid inserting invisible characters
