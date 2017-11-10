984340ebae1c65f65fe58b3c5e49b14545bedd2a
TBG-FR <tbg-le-nolife@hotmail.fr>
2017-11-10 20:19:15 +0100

    PHP Pages - TestPage : Creation

---
+++ php/pages/testpage.php
@@ -0,0 +1,45 @@
+<?php
+
+    // Place here the included/required files instructions
+    require_once('../includes/all.inc.php');
+
+    $a = new Database();
+    var_dump($a);
+
+?>
+
+<!-- 'index.php' ~ Homepage -->
+
+<!doctype html>
+
+<html lang="fr">
+
+    <head>
+        <meta charset="UTF-8">
+
+        <title>QCMaster - TESTS</title>
+
+        <?php //include_once("head.php"); // Make all the CSS & JavaScript links ?>
+
+    </head>
+
+    <body>
+
+        <header>
+            <?php //include_once("header.php"); ?>
+            <?php //include_once("navbar.php"); ?>
+        </header>
+
+        <div class="content">
+            
+            TESTS
+            
+        </div>
+
+        <footer>
+            <?php //include_once("footer.php"); ?>
+        </footer>
+
+    </body>
+
+</html>
