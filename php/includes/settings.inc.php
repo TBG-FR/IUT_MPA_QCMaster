beddecd6d896aa61cff5660f878602aca4411245
TBG-FR <tbg-le-nolife@hotmail.fr>
2017-11-10 23:35:21 +0100

    PHP Includes - Settings : Placeholder for DB Tables Names

--- php/includes/settings.inc.php
+++ php/includes/settings.inc.php
@@ -8,6 +8,12 @@ define("DB_PASS", "");
 define("DB_NAME", "bdd");
 define("DB_CHARSET", "utf8");
 
+/* ----- Database Tables Names ----- */
+define("TABLE_TEST", "qcmaster_Test");
+//define("TABLE_USER", "qcmaster_User");
+//define("TABLE_QCM", "qcmaster_QCM");
+//define("TABLE_QUESTION", "qcmaster_Question");
+
 /* ----- Another Configuration ----- */
 /*
     INSERT CONFIG
