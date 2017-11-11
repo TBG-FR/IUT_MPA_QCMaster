4480752bebf3d4d0e04211edf9eee0e8e0dbe0ea
TBG-FR <tbg-le-nolife@hotmail.fr>
2017-11-11 02:39:10 +0100

    PHP Includes - All : Added Classes & Various

--- php/includes/all.inc.php
+++ php/includes/all.inc.php
@@ -1,7 +1,12 @@
 <?php   // 'all.inc.php' ~ Includes everything (Includes & Classes) so that the php pages will have only one file to Include
 
         require_once('settings.inc.php');
+        require_once('various.inc.php');
 
         require_once('../classes/Database.php');
+        require_once('../classes/User.php');
+        require_once('../classes/QCM.php');
+        require_once('../classes/Question.php');
+        require_once('../classes/Answer.php');
 
 // End of file ~ We don't close the PHP tag here, in order to avoid inserting invisible characters
