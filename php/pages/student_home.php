a6ff27a09630199b8771b74bc07509bfffbc57f1
Garcia Tom-Brian <tom-brian.garcia@etu.univ-lyon1.fr>
2017-11-29 12:25:47 +0100

    PHP Pages - Student Home : Initial File

---
+++ php/pages/student_home.php
@@ -0,0 +1,49 @@
+<?php
+
+    // Place here the included/required files instructions
+require_once('../includes/all.inc.php');
+
+?>
+
+<!-- 'student_home.php' ~ Homepage for Students -->
+
+<!doctype html>
+
+<html lang="fr">
+
+    <head>
+        <meta charset="UTF-8">
+
+        <title>QCMaster - Student Homepage</title>
+
+        <?php
+        /* Makes all the CSS & Javascript links */
+        include_once("../includes/head.inc.php");
+        ?>
+
+    </head>
+
+    <body>
+
+        <header>
+            <?php //include_once("header.php"); ?>
+            <?php //include_once("navbar.php"); ?>
+        </header>
+
+        <div class="content">
+            
+            <p class="title">Student Homepage<br /></p>
+
+            <p class="text">
+                Student Homepage<br />
+            </p>
+            
+        </div>
+
+        <footer>
+            <?php //include_once("footer.php"); ?>
+        </footer>
+
+    </body>
+
+</html>
