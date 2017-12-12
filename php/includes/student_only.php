aa3f4055a88e448351637c5b26034bc091e958ee
Garcia Tom-Brian <tom-brian.garcia@etu.univ-lyon1.fr>
2017-12-12 13:53:09 +0100

    PHP - Includes - Unsure

---
+++ php/includes/student_only.php
@@ -0,0 +1,5 @@
+<?php   // 'student_only.php' ~ Restrict access to pages made for Students
+
+
+
+// End of file ~ We don't close the PHP tag here, in order to avoid inserting invisible characters
---
+++ php/includes/teacher_only.php
@@ -0,0 +1,5 @@
+<?php   // 'teacher_only.php' ~ Restrict access to pages made for Teachers
+
+
+
+// End of file ~ We don't close the PHP tag here, in order to avoid inserting invisible characters
