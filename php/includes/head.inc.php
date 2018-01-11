b337c9f0e51744726f76a6e480152cbef79b3405
Garcia Tom-Brian <tom-brian.garcia@etu.univ-lyon1.fr>
2018-01-11 19:55:19 +0100

    PHP Includes - Head : Modified comments

--- php/includes/head.inc.php
+++ php/includes/head.inc.php
@@ -1,11 +1,11 @@
-<!-- CSS : Bootstrap -->
+<!-- ----- CSS : Bootstrap (LOCAL) ----- -->
 <link href='../../css/bootstrap.css' rel='stylesheet' media='all' type='text/css'>
 
-<!-- CSS : Custom -->
+<!-- ----- CSS : Custom (LOCAL) ----- -->
 <link href='../../css/style.css' rel='stylesheet' media='all' type='text/css'>
 
-<!-- Javascript : jQuery -->
+<!-- ----- Javascript : jQuery (CDN) ----- -->
 <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js'></script>
 
-<!-- Javascript : Bootstrap -->
+<!-- ----- Javascript : Bootstrap (LOCAL) ----- -->
 <script src='../../js/bootstrap.js'></script>
